a96c5b7e385451b9f59c48739318f554024b6166
Victor Hugo <projektvek@gmail.com>
2025-06-16 12:13:23 -0300

    :tada: Commit Inicial

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,32 @@
+function Home() {
+  return (
+    <>
+      <div style={{
+        display: "flex",
+        justifyContent: "center",
+        width: "100vw",
+        }}>
+        <div style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "80vw"
+        }}>
+          <h2>Seja bem vindo!</h2>
+          <p>Sinta-se livre para navegar pelo nosso site</p>
+        </div>
+        <div style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "80vw"
+        }}>
+          <img src="https://i.imgur.com/VpwApCU.png
+  " alt="Imagem" width="400px"/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
